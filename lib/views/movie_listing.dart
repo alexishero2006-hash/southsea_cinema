b7327343f2d7adcfe87f841c3984942e8c5d557a
alexishero2006-hash <alexishero2006@gmail.com>
2026-09-25 12:20:46 +0100

    Chnaged the staless to stateful so i can complete the dropdown menu

--- lib/views/movie_listing.dart
+++ lib/views/movie_listing.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:southsea_cinema/constants.dart';
 import 'package:southsea_cinema/widgets/nav_drawer.dart';
 
-class MovieListing extends StatelessWidget {
+class MovieListing extends StatefulWidget {
   const MovieListing({super.key});
+
+  @override
+  State<MovieListing> createState() => _MovieListingState();
+}
+
+class _MovieListingState extends State<MovieListing> {
+  int _totalPrice = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -36,17 +43,40 @@ class MovieListing extends StatelessWidget {
             SizedBox(height: 25),
             Text(
                 'Southsea Cinema Room',
-              style: TextStyle(fontSize: 15),
+              style: TextStyle(fontSize: 20),
                 ),
             Text(
                'Thursday, 20th June 2027, 20:00 - ends at 22:30',
-             style: TextStyle(fontSize: 15),
-                ),
-            
-        
-        ],
+             style: TextStyle(fontSize: 20),
+                ), 
+                SizedBox(height: 35),
+             Text(
+               'Select tickets(Up to 5 per order) , Membership discount available only at the entrance',
+             style: TextStyle(fontSize: 15),),
+
+              DropdownMenu<int>(
+                initialSelection: 0,
+                helperText: 'Select number of tickets',
+                onSelected: (int? value) {
+                  if (value != null) {
+                    setState(() {
+                      _totalPrice = value;
+                    });
+                  }
+                },
+                dropdownMenuEntries: [
+                  DropdownMenuEntry(value: 1, label: '1'),
+                  DropdownMenuEntry(value: 2, label: '2'),
+                  DropdownMenuEntry(value: 3, label: '3'),
+                  DropdownMenuEntry(value: 4, label: '4'),
+                  DropdownMenuEntry(value: 5, label: '5'),
+                ]
+              )
+   ],
+           
+          ],
+        ),
       ),
-    ),
     );
   }
 }
